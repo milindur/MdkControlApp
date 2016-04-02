e607611ef6deb3a53511476f736a2dd48c5fd689
Christian Andersen <christian@milindur.de>
2016-04-02 10:49:43 +0200

    Updated Assembly-Info

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,12 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Christian Andersen")]
 [assembly: AssemblyProduct("MDK Control")]
-[assembly: AssemblyCopyright("Copyright © 2015 Christian Andersen")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Christian Andersen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
