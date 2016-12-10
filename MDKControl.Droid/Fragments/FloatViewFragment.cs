8d704d6d2d8f486f77c81bfa5f2c516316964a77
Christian Andersen <christian@milindur.de>
2016-12-11 00:21:31 +0100

    Android: Added float view

---
+++ MDKControl.Droid/Fragments/FloatViewFragment.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace MDKControl.Droid.Fragments
+{
+    public class FloatViewFragment : DialogFragment
+    {
+        private Button _closeButton;
+        private Button _cancelButton;
+        private NumberPicker _hundredPicker;
+        private NumberPicker _tenPicker;
+        private NumberPicker _onePicker;
+        private NumberPicker _tenthPicker;
+        private NumberPicker _hundredthsPicker;
+
+        public event EventHandler Canceled;
+        public event EventHandler<float> Closed;
+
+        private string _titleLabel = "Shots";
+        private float _value;
+
+        public static FloatViewFragment NewInstance(string titleLabel, float value) 
+        {
+            var args = new Bundle();
+            args.PutString("titleLabel", titleLabel);
+            args.PutFloat("value", value);
+
+            var f = new FloatViewFragment
+            {
+                Arguments = args,
+                ShowsDialog = true
+            };
+
+            return f;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            _titleLabel = Arguments.GetString("titleLabel");
+            _value = Arguments.GetInt("value");
+
+            SetStyle(DialogFragmentStyle.Normal, 0);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            Dialog.SetTitle(_titleLabel);
+            
+            var view = inflater.Inflate(Resource.Layout.IntegerView, container, false);
+            return view;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            HundredthsPicker.MinValue = 0;
+            HundredthsPicker.MaxValue = 9;
+            TenthPicker.MinValue = 0;
+            TenthPicker.MaxValue = 9;
+            OnePicker.MinValue = 0;
+            OnePicker.MaxValue = 9;
+            TenPicker.MinValue = 0;
+            TenPicker.MaxValue = 9;
+            HundredPicker.MinValue = 0;
+            HundredPicker.MaxValue = 9;
+
+            Value = _value;
+
+            CloseButton.Click += (o, e) => 
+                { 
+                    Closed?.Invoke(this, Value);
+                    Dismiss();  
+                };
+            CancelButton.Click += (o, e) => 
+                { 
+                    Canceled?.Invoke(this, EventArgs.Empty);
+                    Dismiss(); 
+                };
+        }
+
+        protected float Value
+        {
+            get { return HundredPicker.Value * 100 + TenPicker.Value * 10 + OnePicker.Value + TenthPicker.Value / 10f + HundredthsPicker.Value / 100f; }
+            set 
+            {
+                HundredPicker.Value = (int)value / 100;
+                TenPicker.Value = ((int)value % 100) / 10;
+                OnePicker.Value = (int)value % 10;
+            }
+        }
+
+        public NumberPicker HundredthsPicker => _hundredthsPicker ?? (_hundredthsPicker = View.FindViewById<NumberPicker>(Resource.Id.Hundredths));
+
+        public NumberPicker TenthPicker => _tenthPicker ?? (_tenthPicker = View.FindViewById<NumberPicker>(Resource.Id.Tenth));
+
+        public NumberPicker OnePicker => _onePicker ?? (_onePicker = View.FindViewById<NumberPicker>(Resource.Id.One));
+
+        public NumberPicker TenPicker => _tenPicker ?? (_tenPicker = View.FindViewById<NumberPicker>(Resource.Id.Ten));
+
+        public NumberPicker HundredPicker => _hundredPicker ?? (_hundredPicker = View.FindViewById<NumberPicker>(Resource.Id.Hundred));
+
+        public Button CloseButton => _closeButton ?? (_closeButton = View.FindViewById<Button>(Resource.Id.Close));
+
+        public Button CancelButton => _cancelButton ?? (_cancelButton = View.FindViewById<Button>(Resource.Id.Cancel));
+    }
+}
+    
--- MDKControl.Droid/Resources/Resource.designer.cs
+++ MDKControl.Droid/Resources/Resource.designer.cs
@@ -65,11 +65,11 @@ namespace MDKControl.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f07000f
-			public const int Cancel = 2131165199;
+			// aapt resource value: 0x7f070010
+			public const int Cancel = 2131165200;
 			
-			// aapt resource value: 0x7f07000e
-			public const int Close = 2131165198;
+			// aapt resource value: 0x7f07000f
+			public const int Close = 2131165199;
 			
 			// aapt resource value: 0x7f070001
 			public const int ConnectSwitch = 2131165185;
@@ -92,116 +92,116 @@ namespace MDKControl.Droid
 			// aapt resource value: 0x7f070002
 			public const int DisconnectedLayout = 2131165186;
 			
-			// aapt resource value: 0x7f070041
-			public const int DurationTime = 2131165249;
-			
-			// aapt resource value: 0x7f070025
-			public const int ElapsedShots = 2131165221;
-			
-			// aapt resource value: 0x7f070024
-			public const int ElapsedTime = 2131165220;
-			
-			// aapt resource value: 0x7f070038
-			public const int ExposureTime = 2131165240;
-			
-			// aapt resource value: 0x7f070042
-			public const int Hours = 2131165250;
-			
-			// aapt resource value: 0x7f07000b
-			public const int Hundred = 2131165195;
-			
-			// aapt resource value: 0x7f070045
-			public const int Hundredths = 2131165253;
-			
-			// aapt resource value: 0x7f07003a
-			public const int IntervalTime = 2131165242;
-			
-			// aapt resource value: 0x7f070010
-			public const int Joystick = 2131165200;
-			
-			// aapt resource value: 0x7f070020
-			public const int LunarRadioButton = 2131165216;
-			
-			// aapt resource value: 0x7f07003b
-			public const int MaxShots = 2131165243;
-			
-			// aapt resource value: 0x7f070018
-			public const int MdkV5RadioButton = 2131165208;
-			
-			// aapt resource value: 0x7f070019
-			public const int MdkV6RadioButton = 2131165209;
-			
 			// aapt resource value: 0x7f070043
-			public const int Minutes = 2131165251;
-			
-			// aapt resource value: 0x7f07001a
-			public const int NoTRadioButton = 2131165210;
-			
-			// aapt resource value: 0x7f07001d
-			public const int NorthRadioButton = 2131165213;
-			
-			// aapt resource value: 0x7f07000d
-			public const int One = 2131165197;
-			
-			// aapt resource value: 0x7f07001c
-			public const int OtherMechanicsGearReduction = 2131165212;
-			
-			// aapt resource value: 0x7f07001b
-			public const int OtherMechanicsRadioButton = 2131165211;
-			
-			// aapt resource value: 0x7f07002a
-			public const int OverallCols = 2131165226;
-			
-			// aapt resource value: 0x7f07002b
-			public const int OverallRows = 2131165227;
-			
-			// aapt resource value: 0x7f070029
-			public const int OverallShots = 2131165225;
-			
-			// aapt resource value: 0x7f070028
-			public const int OverallTime = 2131165224;
-			
-			// aapt resource value: 0x7f070016
-			public const int PanAxisRadioButton = 2131165206;
-			
-			// aapt resource value: 0x7f070030
-			public const int PanSize = 2131165232;
-			
-			// aapt resource value: 0x7f07002e
-			public const int PanStartPos = 2131165230;
-			
-			// aapt resource value: 0x7f07002f
-			public const int PanStopPos = 2131165231;
-			
-			// aapt resource value: 0x7f070013
-			public const int Pause = 2131165203;
-			
-			// aapt resource value: 0x7f070039
-			public const int PostDelayTime = 2131165241;
-			
-			// aapt resource value: 0x7f070037
-			public const int PreDelayTime = 2131165239;
-			
-			// aapt resource value: 0x7f070023
-			public const int Progress = 2131165219;
+			public const int DurationTime = 2131165251;
 			
 			// aapt resource value: 0x7f070027
-			public const int RemainingShots = 2131165223;
+			public const int ElapsedShots = 2131165223;
 			
 			// aapt resource value: 0x7f070026
-			public const int RemainingTime = 2131165222;
+			public const int ElapsedTime = 2131165222;
 			
-			// aapt resource value: 0x7f070012
-			public const int Resume = 2131165202;
-			
-			// aapt resource value: 0x7f070046
-			public const int ScanningStart = 2131165254;
-			
-			// aapt resource value: 0x7f070047
-			public const int ScanningStop = 2131165255;
+			// aapt resource value: 0x7f07003a
+			public const int ExposureTime = 2131165242;
 			
 			// aapt resource value: 0x7f070044
-			public const int Seconds = 2131165252;
+			public const int Hours = 2131165252;
+			
+			// aapt resource value: 0x7f07000a
+			public const int Hundred = 2131165194;
+			
+			// aapt resource value: 0x7f07000e
+			public const int Hundredths = 2131165198;
+			
+			// aapt resource value: 0x7f07003c
+			public const int IntervalTime = 2131165244;
+			
+			// aapt resource value: 0x7f070012
+			public const int Joystick = 2131165202;
+			
+			// aapt resource value: 0x7f070022
+			public const int LunarRadioButton = 2131165218;
+			
+			// aapt resource value: 0x7f07003d
+			public const int MaxShots = 2131165245;
+			
+			// aapt resource value: 0x7f07001a
+			public const int MdkV5RadioButton = 2131165210;
+			
+			// aapt resource value: 0x7f07001b
+			public const int MdkV6RadioButton = 2131165211;
+			
+			// aapt resource value: 0x7f070045
+			public const int Minutes = 2131165253;
+			
+			// aapt resource value: 0x7f07001c
+			public const int NoTRadioButton = 2131165212;
+			
+			// aapt resource value: 0x7f07001f
+			public const int NorthRadioButton = 2131165215;
+			
+			// aapt resource value: 0x7f07000c
+			public const int One = 2131165196;
+			
+			// aapt resource value: 0x7f07001e
+			public const int OtherMechanicsGearReduction = 2131165214;
+			
+			// aapt resource value: 0x7f07001d
+			public const int OtherMechanicsRadioButton = 2131165213;
+			
+			// aapt resource value: 0x7f07002c
+			public const int OverallCols = 2131165228;
+			
+			// aapt resource value: 0x7f07002d
+			public const int OverallRows = 2131165229;
+			
+			// aapt resource value: 0x7f07002b
+			public const int OverallShots = 2131165227;
+			
+			// aapt resource value: 0x7f07002a
+			public const int OverallTime = 2131165226;
+			
+			// aapt resource value: 0x7f070018
+			public const int PanAxisRadioButton = 2131165208;
+			
+			// aapt resource value: 0x7f070032
+			public const int PanSize = 2131165234;
+			
+			// aapt resource value: 0x7f070030
+			public const int PanStartPos = 2131165232;
+			
+			// aapt resource value: 0x7f070031
+			public const int PanStopPos = 2131165233;
+			
+			// aapt resource value: 0x7f070015
+			public const int Pause = 2131165205;
+			
+			// aapt resource value: 0x7f07003b
+			public const int PostDelayTime = 2131165243;
+			
+			// aapt resource value: 0x7f070039
+			public const int PreDelayTime = 2131165241;
+			
+			// aapt resource value: 0x7f070025
+			public const int Progress = 2131165221;
+			
+			// aapt resource value: 0x7f070029
+			public const int RemainingShots = 2131165225;
+			
+			// aapt resource value: 0x7f070028
+			public const int RemainingTime = 2131165224;
+			
+			// aapt resource value: 0x7f070014
+			public const int Resume = 2131165204;
+			
+			// aapt resource value: 0x7f070047
+			public const int ScanningStart = 2131165255;
+			
+			// aapt resource value: 0x7f070048
+			public const int ScanningStop = 2131165256;
+			
+			// aapt resource value: 0x7f070046
+			public const int Seconds = 2131165254;
 			
 			// aapt resource value: 0x7f070007
 			public const int SetModeAstro = 2131165191;
@@ -212,74 +212,77 @@ namespace MDKControl.Droid
 			// aapt resource value: 0x7f070005
 			public const int SetModeSms = 2131165189;
 			
-			// aapt resource value: 0x7f07002c
-			public const int SetRefStart = 2131165228;
+			// aapt resource value: 0x7f07002e
+			public const int SetRefStart = 2131165230;
 			
-			// aapt resource value: 0x7f07002d
-			public const int SetRefStop = 2131165229;
-			
-			// aapt resource value: 0x7f070034
-			public const int SetStart = 2131165236;
-			
-			// aapt resource value: 0x7f070035
-			public const int SetStop = 2131165237;
-			
-			// aapt resource value: 0x7f07001f
-			public const int SiderealRadioButton = 2131165215;
-			
-			// aapt resource value: 0x7f070011
-			public const int Slider = 2131165201;
-			
-			// aapt resource value: 0x7f070015
-			public const int SliderAxisRadioButton = 2131165205;
-			
-			// aapt resource value: 0x7f07003f
-			public const int SliderStartPos = 2131165247;
-			
-			// aapt resource value: 0x7f070040
-			public const int SliderStopPos = 2131165248;
-			
-			// aapt resource value: 0x7f070021
-			public const int SolarRadioButton = 2131165217;
-			
-			// aapt resource value: 0x7f07001e
-			public const int SouthRadioButton = 2131165214;
-			
-			// aapt resource value: 0x7f070022
-			public const int StartProgram = 2131165218;
-			
-			// aapt resource value: 0x7f070014
-			public const int Stop = 2131165204;
+			// aapt resource value: 0x7f07002f
+			public const int SetRefStop = 2131165231;
 			
 			// aapt resource value: 0x7f070036
-			public const int SwapStartStop = 2131165238;
+			public const int SetStart = 2131165238;
 			
-			// aapt resource value: 0x7f07000c
-			public const int Ten = 2131165196;
+			// aapt resource value: 0x7f070037
+			public const int SetStop = 2131165239;
 			
-			// aapt resource value: 0x7f07000a
-			public const int Thousand = 2131165194;
+			// aapt resource value: 0x7f070021
+			public const int SiderealRadioButton = 2131165217;
+			
+			// aapt resource value: 0x7f070013
+			public const int Slider = 2131165203;
 			
 			// aapt resource value: 0x7f070017
-			public const int TiltAxisRadioButton = 2131165207;
+			public const int SliderAxisRadioButton = 2131165207;
+			
+			// aapt resource value: 0x7f070041
+			public const int SliderStartPos = 2131165249;
+			
+			// aapt resource value: 0x7f070042
+			public const int SliderStopPos = 2131165250;
+			
+			// aapt resource value: 0x7f070023
+			public const int SolarRadioButton = 2131165219;
+			
+			// aapt resource value: 0x7f070020
+			public const int SouthRadioButton = 2131165216;
+			
+			// aapt resource value: 0x7f070024
+			public const int StartProgram = 2131165220;
+			
+			// aapt resource value: 0x7f070016
+			public const int Stop = 2131165206;
+			
+			// aapt resource value: 0x7f070038
+			public const int SwapStartStop = 2131165240;
+			
+			// aapt resource value: 0x7f07000b
+			public const int Ten = 2131165195;
+			
+			// aapt resource value: 0x7f07000d
+			public const int Tenth = 2131165197;
+			
+			// aapt resource value: 0x7f070011
+			public const int Thousand = 2131165201;
+			
+			// aapt resource value: 0x7f070019
+			public const int TiltAxisRadioButton = 2131165209;
+			
+			// aapt resource value: 0x7f070035
+			public const int TiltSize = 2131165237;
 			
 			// aapt resource value: 0x7f070033
-			public const int TiltSize = 2131165235;
+			public const int TiltStartPos = 2131165235;
 			
-			// aapt resource value: 0x7f070031
-			public const int TiltStartPos = 2131165233;
-			
-			// aapt resource value: 0x7f070032
-			public const int TiltStopPos = 2131165234;
-			
-			// aapt resource value: 0x7f07003c
-			public const int VideoLength24 = 2131165244;
-			
-			// aapt resource value: 0x7f07003d
-			public const int VideoLength25 = 2131165245;
+			// aapt resource value: 0x7f070034
+			public const int TiltStopPos = 2131165236;
 			
 			// aapt resource value: 0x7f07003e
-			public const int VideoLength30 = 2131165246;
+			public const int VideoLength24 = 2131165246;
+			
+			// aapt resource value: 0x7f07003f
+			public const int VideoLength25 = 2131165247;
+			
+			// aapt resource value: 0x7f070040
+			public const int VideoLength30 = 2131165248;
 			
 			static Id()
 			{
@@ -301,34 +304,37 @@ namespace MDKControl.Droid
 			public const int DeviceView = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int IntegerView = 2130903042;
+			public const int FloatView = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int JoystickView = 2130903043;
+			public const int IntegerView = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int ModeAstroStatusView = 2130903044;
+			public const int JoystickView = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int ModeAstroView = 2130903045;
+			public const int ModeAstroStatusView = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int ModePanoStatusView = 2130903046;
+			public const int ModeAstroView = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int ModePanoView = 2130903047;
+			public const int ModePanoStatusView = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int ModeSmsStatusView = 2130903048;
+			public const int ModePanoView = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int ModeSmsView = 2130903049;
+			public const int ModeSmsStatusView = 2130903049;
 			
 			// aapt resource value: 0x7f03000a
-			public const int Splash = 2130903050;
+			public const int ModeSmsView = 2130903050;
 			
 			// aapt resource value: 0x7f03000b
-			public const int TimeView = 2130903051;
+			public const int Splash = 2130903051;
+			
+			// aapt resource value: 0x7f03000c
+			public const int TimeView = 2130903052;
 			
 			static Layout()
 			{
